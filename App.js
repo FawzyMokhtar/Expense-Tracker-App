f8dfde95ca0bb7118612e9e775d4d24ab913615f
FawzyMokhtar <dev.fawzymokhtar@gmail.com>
2022-08-24 15:37:23 +0300

    Add state-management.

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
+import { StyleSheet } from 'react-native';
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+
+import { store } from './store';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style='auto' />
-    </View>
+    <>
+      <StatusBar style='light' />
+      <Provider store={store}></Provider>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
---
+++ store/redux/reducers/expenses.js
@@ -0,0 +1,51 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+import { EXPENSES } from '../../../data';
+import { Expense } from '../../../models';
+
+const expensesSlice = createSlice({
+  name: 'expenses',
+  initialState: {
+    data: EXPENSES,
+  },
+  reducers: {
+    create: (state, action) => {
+      const { description, value } = action.payload;
+      const expense = new Expense(description, value);
+
+      state.data.push(expense);
+    },
+    update: (state, action) => {
+      const { id, description, value } = action.payload;
+      const expense = state.data.find((expense) => expense.id === id);
+
+      if (expense) {
+        expense = {
+          ...expense,
+          description,
+          value,
+        };
+      }
+    },
+    delete: (state, action) => {
+      const id = action.payload.id;
+      const expenseIndex = state.data.indexOf((expense) => expense.id === id);
+
+      if (expenseIndex !== -1) {
+        state.data.splice(expenseIndex, 1);
+      }
+    },
+  },
+});
+
+export const ExpensesActions = {
+  create: expensesSlice.actions.create,
+  update: expensesSlice.actions.update,
+  delete: expensesSlice.actions.delete,
+};
+
+export const ExpensesSelectors = {
+  getExpenses: (state) => state.data,
+};
+
+export default expensesSlice.reducer;
