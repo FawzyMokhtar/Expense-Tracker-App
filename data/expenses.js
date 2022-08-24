3f2bc2e1b882373a2ed8efd8206bdb804f41479d
FawzyMokhtar <dev.fawzymokhtar@gmail.com>
2022-08-24 14:25:26 +0300

    Add project models & data.

---
+++ data/expenses.js
@@ -0,0 +1,10 @@
+import { Expense } from '../models';
+
+export const EXPENSES = [
+  new Expense('A book', 10.45),
+  new Expense('Another book', 15.0),
+  new Expense('Mobile charger', 20.0),
+  new Expense('Tissues', 21.0),
+  new Expense('Food', 10.0),
+  new Expense('Electricity bill', 30.0),
+];
---
+++ models/expense.js
@@ -0,0 +1,10 @@
+import uuid from 'react-native-uuid';
+
+export class Expense {
+  constructor(description, value) {
+    this.id = uuid.v4();
+    this.description = description;
+    this.value = value;
+    this.createdAt = new Date(Date.now);
+  }
+}
